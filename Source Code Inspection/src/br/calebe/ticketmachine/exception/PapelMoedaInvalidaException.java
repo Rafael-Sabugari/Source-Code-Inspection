adb4beb3bd6c3b95a74a3f0524a5b79765be74ec
Enzo Tatiyama <enzo.tatiyama@gmail.com>
2023-09-28 22:21:46 -0300

    fix: exception files

--- Source Code Inspection/src/br/calebe/ticketmachine/exception/PapelMoedaInvalidaException.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/exception/PapelMoedaInvalidaException.java
@@ -5,5 +5,11 @@ package br.calebe.ticketmachine.exception;
  * @author Calebe de Paula Bianchini
  */
 public class PapelMoedaInvalidaException extends Exception {
-    
+    public PapelMoedaInvalidaException() {
+        super("PapelMoedaInvalidaException occurred");
+    }
+
+    public PapelMoedaInvalidaException(String message) {
+        super(message);
+    }
 }
--- Source Code Inspection/src/br/calebe/ticketmachine/exception/SaldoInsuficienteException.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/exception/SaldoInsuficienteException.java
@@ -5,4 +5,11 @@ package br.calebe.ticketmachine.exception;
  * @author Calebe de Paula Bianchini
  */
 public class SaldoInsuficienteException extends Exception {
+    public SaldoInsuficienteException() {
+        super("SaldoInsuficienteException occurred");
+    }
+
+    public SaldoInsuficienteException(String message) {
+        super(message);
+    }
 }
